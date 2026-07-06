90e0d8d665a8b2d8a6f7d3d86ba6a2e8e7302621
Jerome Roque <jgroque26@gmail.com>
2026-07-06 15:50:22 +0800

    chore(): fix cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,10 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = [
-    process.env.FRONTEND_URL     ?? 'http://localhost:3000',  // Next.js
-    process.env.FRONTEND_ANGULAR_URL ?? 'http://localhost', // Angular
-  ];
+  const allowedOrigins = (process.env.FRONTEND_URL ?? 'http://localhost:3000,http://localhost').split(',');
 
   app.enableCors({
     origin: (origin, callback) => {
